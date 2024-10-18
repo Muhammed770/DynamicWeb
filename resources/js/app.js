b71a166515d62cbcc4c26ad5a348386613648187
muhammed770 <mail.muhammed2002@gmail.com>
2024-10-18 19:11:11 +0400

    copy to clipboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,20 @@
 import './bootstrap';
+import ClipboardJS from 'clipboard';
 
 import.meta.globe([
     '../images/**'
 ])
+
+new ClipboardJS('.copy-to-clipboard');
+
+// Optional: Add success and error event listeners
+var clipboard = new ClipboardJS('.copy-to-clipboard');
+
+clipboard.on('success', function(e) {
+    console.log('Text copied:', e.text);
+    e.clearSelection(); // Optionally deselect the text
+});
+
+clipboard.on('error', function(e) {
+    console.error('Failed to copy:', e);
+});
--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -13,8 +13,10 @@
     <div class=" max-md:hidden right-0 border border-s-2 ">
         <div class="px-4 py-2 min-w-44">
             <h2>Api end point</h2>
-            <p class="block rounded-lg shadow-sm hover:bg-gray-100 border bg-gray-200 px-4 py-2 text-sm font-medium text-gray-700 ">
-                {{$page->slug}}
+            <p class="block rounded-lg shadow-sm hover:bg-gray-100 border bg-gray-200 px-4 py-2 text-sm font-medium text-gray-700 copy-to-clipboard"
+            data-clipboard-text="{{url('/api/pages/'.$page->slug)}}"
+            >
+            {{url('/api/pages/'.$page->slug)}}
             </p>
         </div>
     </div>
